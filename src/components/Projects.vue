14f6421e25ee53ebbbfdb3f6ee300f2188f7537b
nehry <enehry@gmail.com>
2023-05-23 14:31:38 +0800

    fix alignment

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,34 +3,36 @@
     <!-- <div class="inline-flex items-center justify-center w-full gap-8"> -->
     <div class="mb-24">
       <!-- <hr class="w-64 h-1 bg-gray-200 border-0 rounded-full dark:bg-gray-700" /> -->
-      <h1 class="text-4xl font-semibold text-center dark:text-gray-300">
+      <h1 class="text-center text-4xl font-semibold dark:text-gray-300">
         Projects
       </h1>
       <!-- <hr class="w-64 h-1 bg-gray-200 border-0 rounded-full dark:bg-gray-700" /> -->
-      <p class="text-center dark:text-gray-400 text-gray-500">
+      <p class="text-center text-gray-500 dark:text-gray-400">
         Here are some of the projects I've worked with.
       </p>
     </div>
-    <div class="max-w-5xl mx-auto">
-      <div class="grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-8">
+    <div class="mx-auto max-w-5xl">
+      <div
+        class="grid place-items-start justify-items-center gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
+      >
         <div class="space-y-2" v-for="project in projects" :key="project.order">
-          <div class="relative group">
+          <div class="group relative">
             <div
-              class="opacity-0 z-10 group-hover:opacity-50 transition-all duration-500 ease-in-out absolute items-center justify-center w-full h-full bg-black rounded-md"
+              class="absolute z-10 h-full w-full items-center justify-center rounded-md bg-black opacity-0 transition-all duration-500 ease-in-out group-hover:opacity-50"
             ></div>
             <div
-              class="space-y-2 opacity-0 z-20 group-hover:opacity-100 absolute transition-all duration-500 ease-in-out top-0 right-0 bottom-0 left-0 m-auto h-min w-min text-white"
+              class="absolute bottom-0 left-0 right-0 top-0 z-20 m-auto h-min w-min space-y-2 text-white opacity-0 transition-all duration-500 ease-in-out group-hover:opacity-100"
             >
               <div v-if="project.github">
                 <a
-                  class="flex justify-center py-1 gap-2 items-center hover:underline rounded-full hover:px-2 hover:border-white transition-all duration-500 ease-in-out"
+                  class="flex items-center justify-center gap-2 rounded-full py-1 transition-all duration-500 ease-in-out hover:border-white hover:px-2 hover:underline"
                   :href="project.github"
                   target="_blank"
                 >
                   <svg
                     role="img"
                     viewBox="0 0 24 24"
-                    class="w-6 h-6 fill-current"
+                    class="h-6 w-6 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <title>GitHub</title>
@@ -43,7 +45,7 @@
               </div>
               <div v-if="project.link">
                 <a
-                  class="flex justify-center py-1 gap-2 items-center hover:underline rounded-full hover:px-2 hover:border-white transition-all duration-500 ease-in-out"
+                  class="flex items-center justify-center gap-2 rounded-full py-1 transition-all duration-500 ease-in-out hover:border-white hover:px-2 hover:underline"
                   :href="project.link"
                   target="_blank"
                 >
@@ -51,7 +53,7 @@
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="currentColor"
-                    class="w-5 h-5"
+                    class="h-5 w-5"
                   >
                     <path
                       d="M12.232 4.232a2.5 2.5 0 013.536 3.536l-1.225 1.224a.75.75 0 001.061 1.06l1.224-1.224a4 4 0 00-5.656-5.656l-3 3a4 4 0 00.225 5.865.75.75 0 00.977-1.138 2.5 2.5 0 01-.142-3.667l3-3z"
@@ -65,7 +67,7 @@
               </div>
             </div>
             <div
-              class="relative max-w-xs overflow-hidden bg-cover bg-no-repeat rounded-md"
+              class="relative max-w-xs overflow-hidden rounded-md bg-cover bg-no-repeat"
             >
               <img
                 class="max-w-xs transition duration-500 ease-in-out group-hover:scale-110"
@@ -74,14 +76,14 @@
               />
             </div>
           </div>
-          <div class="space-y-2">
+          <div class="max-w-xs space-y-2">
             <h2 class="text-lg font-semibold dark:text-gray-300">
               {{ project.title }}
             </h2>
             <ul class="flex flex-wrap justify-start gap-1">
               <li v-for="tag in project.tags" :key="tag">
                 <span
-                  class="px-2 py-1 text-xs text-gray-900 dark:text-gray-300 border-2 border-gray-500 rounded-full"
+                  class="rounded-full border border-gray-500 px-2 py-1 text-xs text-gray-900 dark:text-gray-300"
                   >{{ tag }}</span
                 >
               </li>
@@ -94,7 +96,7 @@
 </template>
 
 <script setup lang="ts">
-import { Asset, ContentfulClientApi } from "contentful";
+import { ContentfulClientApi } from "contentful";
 // import { Project } from "@/interfaces";
 import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
 import type { Document } from "@contentful/rich-text-types";
